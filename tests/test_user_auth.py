6d0bdb112605c7c4db1b63e6f7016f49def589a8
SashaShulha <sashamiller666@gmail.com>
2022-07-16 23:25:02 +0300

    added test_user_auth.py

---
+++ tests/test_user_auth.py
@@ -0,0 +1,53 @@
+from lib.assertions import Assertions
+from lib.base_case import BaseCase
+from lib.my_requests import MyRequests
+import pytest
+
+
+class TestUserAuth(BaseCase):
+    exclude_params = [
+        ('no_cookie'),
+        ('no_token')
+    ]
+
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        first_response = MyRequests.post('/user/login', data=data)
+
+        self.auth_sid = self.get_cookie(first_response, 'auth_sid')
+        self.token = self.get_header(first_response, 'x-csrf-token')
+        self.user_id_from_auth_method = self.get_json_value(first_response, 'user_id')
+
+    def test_auth_user(self):
+
+        second_response = MyRequests.get(
+            '/user/auth',
+            headers={'x-csrf-token': self.token},
+            cookies={'auth_sid': self.auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            second_response,
+            'user_id',
+            self.user_id_from_auth_method,
+            'User id from auth method is not equal to user id from check method'
+        )
+
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+
+        if condition == 'no_cookie':
+            second_response = MyRequests.get('/user/auth', headers={'x-csrf-token': self.token})
+        else:
+            second_response = MyRequests.get('/user/auth', cookies={'auth_sid': self.auth_sid})
+
+        Assertions.assert_json_value_by_name(
+            second_response,
+            'user_id',
+            0,
+            f"User is authorized with condition {condition}"
+        )
